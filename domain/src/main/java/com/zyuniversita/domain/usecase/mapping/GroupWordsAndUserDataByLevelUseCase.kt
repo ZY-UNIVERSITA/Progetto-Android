84ec69ee5f549529c97c1cd29d33a40a429be343
ZY <yuhang.zhu@studio.unibo.it>
2025-05-22 02:21:31 +0200

    Add function to group list of word by tag

---
+++ domain/src/main/java/com/zyuniversita/domain/usecase/mapping/GroupWordsAndUserDataByLevelUseCase.kt
@@ -0,0 +1,19 @@
+package com.zyuniversita.domain.usecase.mapping
+
+import com.zyuniversita.domain.model.WordProgress
+import javax.inject.Inject
+
+interface GroupWordsAndUserDataByLevelUseCase : (List<WordProgress>) -> MutableList<MutableList<WordProgress>>
+
+class GroupWordsAndUserDataByLevelUseCaseImpl @Inject constructor() : GroupWordsAndUserDataByLevelUseCase {
+    override fun invoke(wordList: List<WordProgress>): MutableList<MutableList<WordProgress>> {
+        // create a map using levelCode as a key and sort the by levelCode
+        // In each map, sort the list by wordId and return a mutableList
+        // Create a mutable list from the map
+        val list = wordList.groupBy { it.word.levelCode }
+            .toSortedMap().values.map { it.sortedBy { data -> data.word.wordId }.toMutableList() }
+            .toMutableList()
+
+        return list
+    }
+}
---
+++ domain/src/main/java/com/zyuniversita/domain/usecase/mapping/GroupWordsByLevelUseCase.kt
@@ -0,0 +1,17 @@
+package com.zyuniversita.domain.usecase.mapping
+
+import com.zyuniversita.domain.model.Word
+import javax.inject.Inject
+
+interface GroupWordsByLevelUseCase : (List<Word>) -> MutableList<MutableList<Word>>
+
+class GroupWordsByLevelUseCaseImpl @Inject constructor() : GroupWordsByLevelUseCase {
+    override fun invoke(wordsList: List<Word>): MutableList<MutableList<Word>> {
+        val list = wordsList.groupBy { it.levelCode }
+            .toSortedMap().values.map { it.sortedBy { data -> data.wordId }.toMutableList() }
+            .toMutableList()
+
+        return list
+    }
+
+}
