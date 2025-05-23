3ef24f80c72a0ad90518d16d4696f2a0c78e5244
ZY <yuhang.zhu@studio.unibo.it>
2025-05-23 02:06:28 +0200

    Add homepage fragment

---
+++ ui/src/main/java/com/zyuniversita/ui/main/homepage/HomePageFragment.kt
@@ -0,0 +1,46 @@
+package com.zyuniversita.ui.main.homepage
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.zyuniversita.ui.databinding.HomepageMainFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomePageFragment: Fragment() {
+    private var _binding: HomepageMainFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private val TAG: String = "Homepage Main fragment TAG"
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        Log.d(TAG, "Fragment created")
+
+        _binding = HomepageMainFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        Log.d(TAG, "Fragment destroyed")
+
+        _binding = null
+    }
+
+
+}
