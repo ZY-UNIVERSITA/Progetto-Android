9cc2aeb136f4c8c6441562021992f592430b0234
ZY <yuhang.zhu@studio.unibo.it>
2025-06-02 20:25:24 +0200

    Add homepage instructions

--- ui/src/main/java/com/zyuniversita/ui/main/homepage/HomePageFragment.kt
+++ ui/src/main/java/com/zyuniversita/ui/main/homepage/HomePageFragment.kt
@@ -5,9 +5,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.zyuniversita.ui.databinding.FragmentHomepageBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomePageFragment: Fragment() {
@@ -15,6 +19,8 @@ class HomePageFragment: Fragment() {
     private val binding get() = _binding!!
 
     private val TAG: String = "Homepage Main fragment TAG"
+
+    private val viewModel: HomePageViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +37,11 @@ class HomePageFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch {
+            val words = viewModel.readWords()
+            binding.instructionsText.text = HtmlCompat.fromHtml(words, HtmlCompat.FROM_HTML_MODE_LEGACY)
+        }
 
     }
 
